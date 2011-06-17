8d1c021c7867f17bde17b9206da55035353d78c6
Kristian Rietveld <kris@lanedo.com>
2011-06-17 22:59:12 +0200

    Make make-oxt.sh detect whether the SO is 32 or 64 bit

--- make-oxt.sh
+++ make-oxt.sh
@@ -1,9 +1,16 @@
 #! /bin/sh
 
+# From the ELF object, find out whether it contains 32 or 64 bit code.
+bits=`file build/webdavui.uno.so | sed -e "s/^.*\([0-9][0-9]\)-bit.*$/\1/"`
+
 mkdir dist
 mkdir dist/META-INF
 mkdir dist/images
-mkdir dist/Linux_x86
+if [ $bits -eq 32 ]; then
+	mkdir dist/Linux_x86
+elif [ $bits -eq 64 ]; then
+	mkdir dist/Linux_x86_64
+fi
 
 cp data/Addons.xcu dist
 cp description.xml dist
@@ -11,7 +18,11 @@ cp data/package-description.txt dist
 cp data/ProtocolHandler.xcu dist
 cp data/images/* dist/images
 cp build/manifest.xml dist/META-INF
-cp build/webdavui.uno.so dist/Linux_x86
+if [ $bits -eq 32 ]; then
+	cp build/webdavui.uno.so dist/Linux_x86
+elif [ $bits -eq 64 ]; then
+	cp build/webdavui.uno.so dist/Linux_x86_64
+fi
 
 rm webdavui.oxt
 
